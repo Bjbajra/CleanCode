4efc73b3e334e6562aa3164bc99bf69b0fbb3b54
Bjbajra <70744118+Bjbajra@users.noreply.github.com>
2021-12-20 12:46:43 +0100

    TestForEmptyStringAndOneNumber

---
+++ StringCalculatorTDD.Tests/StringCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorTDD.Tests
+{ 
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        private StringCalculator _calculator = new StringCalculator();
+        [Test]
+        public void StringCalculator_GivenStringIsEmpty_Returns0()
+        {  
+            //Arrange
+            var expectedResult = 0;
+
+            //Act
+            var calculatedResult = _calculator.Add("");
+
+            //Assert
+            Assert.That(expectedResult, Is.EqualTo(calculatedResult));
+
+        }
+        
+        [Test]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void StringCalculator_GivenStringWithOneNumber_ReturnsNumber(string numbers, int expectedResult)
+        {  
+            //Arrange
+            
+            //Act
+            var calculatedResult = _calculator.Add(numbers);
+
+            //Assert
+            Assert.That(expectedResult, Is.EqualTo(calculatedResult));
+
+        }
+        
+    }
+}
---
+++ StringCalculatorTDD/StringCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorTDD
+{
+    public class StringCalculator
+    {
+        public object Add(string numbers)
+        {
+            if (String.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            return int.Parse(numbers);
+        }
+    }
+}
